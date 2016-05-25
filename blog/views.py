cc7fb4297c807f9d04a2d464e31412d112e06b53
ntntktk <afamiliar22@gmail.com>
2016-05-25 15:11:16 +0900

    Post 클래스 생성 및 migrate, index.html 작성, views.py index 메소드 작성,

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def index(request):
+    post_list = Post.objects.all()
+    return render(request, 'blog/index.html', {
+        'post_list': post_list,
+        })
